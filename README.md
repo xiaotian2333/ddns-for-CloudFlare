e900879c92fdb0780e9252274d2a98c5d9e3667e
xiaotian <1719549416@qq.com>
2023-07-14 18:35:26 +0800

    本地化适配

--- README.md
+++ README.md
@@ -1,29 +1,35 @@
-# DdnsOnCloudFlare
-A shell script to automatic update cloudFlare dns record whit actual external ip address, leveraging the official CloudFlare api.
+# CloudFlare DDNS
+一个基于 CloudFlare api 的 shell 脚本，自动更新 ipv6 的 dns 记录
 
-# Guide
-## Pre-work
-- Have a domain and use Cloudflare's nameserver.  
-- Find your `ZoneId` from the [overview] page of you domain.  
-- Create an `api token`. You can find it from [Here](https://dash.cloudflare.com/profile/api-tokens)
-- Clone this project to local, fill in `config.conf`. In which, `zoneId` and `apiKey` refer to the contents you just got in the above steps. `recordName`is the dns record name you'd like to operate.
+# 开始使用
+## 前期准备
+- 拥有一个域名并使用 Cloudflare 的 DNS 服务器。
+- 访问 [CloudFlare](https://dash.cloudflare.com/) 获取你的区域 ID
+- 在[此处](https://dash.cloudflare.com/profile/api-tokens)创建一个 `api token` 
+- 下载 `updateIpv6.sh` 并根据注释修改 `zoneId` `recordName` `apiKey` 字段
 
-## Update Ipv4 record
-Simply run:
-```shell
-sh ./updateIpv4.sh
-```
-This will first get your external ipv4 address, and synchronize to the dns record.  
-If no such record exists, will create one.  
-If the record is already set as the expected ip, will do nothing.
-Or, the record will be updated.
-## Update Ipv6 record
-Similar to the previous, but just run:
-```shell
+## 更新 Ipv6 记录
+
+运行:
+
+``` shell
 sh ./updateIpv6.sh
 ```
-The behavior is also the same.
 
-# Note
-- You can make it periodically executed using [crontab](https://linuxconfig.org/linux-crontab-reference-guide).  
-- DO NOT abuse. According to the term of cloudflare, the api call rate is limited to 1200 requests every 5 minutes. 
+这将首先通过网卡获取 ipv6 地址，并同步到 dns 记录。
+
+如果不存在此类记录，将创建一个。
+
+如果记录已设置为预期的 ip，则不执行任何操作。
+
+# 注意
+
+您可以使用 [crontab](https://linuxconfig.org/linux-crontab-reference-guide) 或 [宝塔](https://www.bt.cn/new/index.html) 定期执行它。
+
+不要滥用，cloudflare 限制 API 调用速率限制为每 5 分钟 1200 个请求。
+
+本项目基于 [imki911/DdnsOnCloudFlare](https://github.com/imki911/DdnsOnCloudFlare) 修改而来，汉化了文档及注释，并修改为单文件以方便在宝塔面板中使用
+
+鉴于国内基本不存在 ipv4 公网，因此去除了 ipv4 的部分，如有需要可使用[原项目](https://github.com/imki911/DdnsOnCloudFlare)
+
+此项目使用 GPL-3.0 许可证 开源
--- updateIpv6.sh
+++ updateIpv6.sh
@@ -1,27 +1,80 @@
 #!/bin/sh
-#title           :updateIpv6.sh
-#description     :
-#author          :gf@gfshen.cn
-#date            :2022-05-03
-#==============================================================================
-if [ -n "$BASH_SOURCE" ]; then
-  workDir=$(
-    cd $(dirname "$BASH_SOURCE")
-    pwd
-  )
-else
-  workDir=$(
-    cd $(dirname $0)
-    pwd
-  )
-fi
-. "$workDir"/base.sh
 
-checkConfValid
-if [ $? -eq 1 ]; then
-  echo "Missing param, please check config.conf "
-  exit
-fi
+# 区域 ID,在域名概述内可以找到
+zoneId=''
+# 要修改的域名，不支持使用如 @.xt-url.com 我也不知道为什么（
+recordName='www.xt-url.com'
+# 你的 API 密钥
+apiKey=''
+
+getIpv6Address() {
+  # IPv6地址获取，一般不用改
+  # 因为一般ipv6没有nat ipv6的获得可以本机获得
+  # ip addr show $(ip route show default | awk '/default/ {print $5}') | awk '/inet6 / {print $2}' | awk -F/ '{print $1}' | grep -v fe80:: | grep -v ::1 | head -n 1 获取第一个
+  ip addr show $(ip route show default | awk '/default/ {print $5}') | awk '/inet6 / {print $2}' | awk -F/ '{print $1}' | grep -v fe80:: | grep -v ::1 | tail -n 1 # 获取最后一个
+  # 通过api获取，可以使用自己的，只要返回的是纯文本就行
+  # curl -s -6 https://ifconfig.co/ip
+  # curl https://api64.ipify.org
+}
+
+# 下面的就别乱改了
+
+listRecord() {
+  local zoneId=$1
+  local recordName=$2
+  local apiKey=$3
+  local result=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneId/dns_records?name=$recordName" \
+    -H "Content-Type:application/json" \
+    -H "Authorization: Bearer $apiKey")
+
+  local resourceId=$(echo "$result" | grep -Po '(?<="id":")[^"]+')
+  local currentValue=$(echo "$result" | grep -Po '(?<="content":")[^"]+')
+
+  local successStat=$(echo "$result" | grep -Po '(?<="success":)[^,]+')
+  if [ "$successStat" != "true" ]; then
+    return 1
+  fi
+
+  printf '%s\n%s' "$resourceId" "$currentValue"
+}
+
+updateRecord() {
+  local zoneId=$1
+  local recordName=$2
+  local apiKey=$3
+  local resourceId=$4
+  local type=$5
+  local value=$6
+
+  local result=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneId/dns_records/$resourceId" \
+    -H "Authorization: Bearer $apiKey" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"$type\",\"name\":\"$recordName\",\"content\":\"$value\",\"ttl\":600,\"proxied\":false}")
+  local successStat=$(echo "$result" | grep -Po '(?<="success":)[^,]+')
+  [ "$successStat" = "true" ]
+  return $?
+}
+
+createRecord() {
+  local zoneId=$1
+  local recordName=$2
+  local apiKey=$3
+  local type=$4
+  local value=$5
+
+  local result=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/$zoneId/dns_records" \
+    -H "Authorization: Bearer $apiKey" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"$type\",\"name\":\"$recordName\",\"content\":\"$value\",\"ttl\":600,\"proxied\":false}")
+  local successStat=$(echo "$result" | grep -Po '(?<="success":)[^,]+')
+  if [ "$successStat" != "true" ]; then
+    return 1
+  fi
+  local recordId=$(echo "$result" | grep -Po '(?<="id":")[^"]+')
+  echo "$recordId"
+}
+
+#start
 
 externalIpv6Add=$(getIpv6Address)
 echo "Get external ipv6 address: $externalIpv6Add"
